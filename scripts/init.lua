d98ce03023c3f0015dd628e35efec0273c211d3c
DDogeOneeSama <87510203+DDogeOneeSama@users.noreply.github.com>
2026-04-06 10:31:17 -0500

    Don't load logic until I am done with it

--- scripts/init.lua
+++ scripts/init.lua
@@ -4,7 +4,7 @@ local variant = Tracker.ActiveVariantUID
 require("scripts/items_import")
 
 -- Logic
-require("scripts/logic/base_logic")
+--require("scripts/logic/base_logic")
 
 -- Maps
 if Tracker.ActiveVariantUID == "maps-u" then
