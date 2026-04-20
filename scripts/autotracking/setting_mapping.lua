8e0cf434f01ca3abd8bb8d0c63ef625a7c282bfb
DDogeOneeSama <ddogcoolthing@gmail.com>
2026-04-19 22:41:15 -0500

    Map slot_data values to setting_item stages

---
+++ scripts/autotracking/setting_mapping.lua
@@ -0,0 +1,250 @@
+SLOT_CODES =
+{
+  gear_rarity_item_pool =
+  {
+    code = "gear_rarity_item_pool",
+    mapping =
+    {
+      [0] = 0, -- Disabled
+      [1] = 1, -- Seraph+ Disabled
+      [2] = 2, -- Pearl+ Disabled
+      [3] = 3, -- Effervescent Disabled
+      [4] = 4  -- All 
+    }
+  },
+  vault_symbols =
+  {
+    code = "vault_symbols",
+    mapping =
+    {
+      [0] = 0, -- Off
+      [1] = 1  -- On
+    }
+  },
+  vending_machines =
+  {
+    code = "vending_machines",
+    mapping =
+    {
+      [0] = 0, -- Off
+      [1] = 1  -- On
+    }
+  },
+  entrance_locks =
+  {
+    code = "entrance_locks",
+    mapping =
+    {
+      [0] = 0, -- Off
+      [1] = 1  -- On
+    }
+  },
+  jump_checks =
+  {
+    code = "jump_checks",
+    mapping =
+    {
+      [0] = 0, -- No Checks
+      [1] = 1, -- 1
+      [2] = 2, -- 2
+      [3] = 3, -- 3
+      [4] = 4, -- 4
+      [5] = 5  -- 5
+    }
+  },
+  max_jump_height =
+  {
+    code = "gear_rarity_item_pool",
+    mapping =
+    {
+      [0] = 0, -- Regular
+      [1] = 1, -- High
+      [2] = 2  -- Extra High
+    }
+  },
+  sprint_checks =
+  {
+    code = "sprint_checks",
+    mapping =
+    {
+      [0] = 0, -- No Checks
+      [1] = 1, -- 1
+      [2] = 2, -- 2
+      [3] = 3, -- 3
+      [4] = 4, -- 4
+      [5] = 5  -- 5
+    }
+  },
+  max_sprint_speed =
+  {
+    code = "max_sprint_speed",
+    mapping =
+    {
+      [0] = 0, -- Regular
+      [1] = 1, -- Fast
+      [2] = 2, -- Extra Fast
+      [4] = 3  -- Supersonic
+    }
+  },
+  quest_completion_checks =
+  {
+    code = "quest_completion_checks",
+    mapping =
+    {
+      [0] = 0, -- None
+      [1] = 1, -- All
+      [2] = 2, -- Story Only
+      [3] = 3  -- Sidequest Only
+    }
+  },
+  generic_mob_checks =
+  {
+    code = "generic_mob_checks",
+    mapping =
+    {
+      [0] = 0, -- Disabled
+      [1] = 1, -- Enabled
+      [2] = 1,
+      [3] = 1,
+      [4] = 1,
+      [5] = 1,
+      [6] = 1,
+      [7] = 1,
+      [8] = 1,
+      [9] = 1,
+      [10] = 1
+    }
+  },
+  gear_rarity_checks =
+  {
+    code = "gear_rarity_checks",
+    mapping =
+    {
+      [0] = 0, -- Disabled
+      [1] = 1, -- Seraph+ Disabled
+      [2] = 2, -- Pearl+ Disabled
+      [3] = 3, -- Effervescent Disabled
+      [4] = 4  -- All On
+    }
+  },
+  challenge_checks =
+  {
+    code = "challenge_checks",
+    mapping =
+    {
+      [0] = 0, -- Disabled
+      [1] = 1, -- All
+      [2] = 2, -- Region-Based only
+      [3] = 3  -- General only
+    }
+  },
+  chest_checks =
+  {
+    code = "chest_checks",
+    mapping =
+    {
+      [0] = 0, -- Disabled
+      [1] = 1  -- Enabled
+    }
+  },
+  remove_missable_checks =
+  {
+    code = "remove_missable_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_coop_checks =
+  {
+    code = "remove_coop_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1, -- Remove Impossible
+      [2] = 2  -- Remove All
+    }
+  },
+  remove_ffs_checks =
+  {
+    code = "remove_ffs_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_tina_checks =
+  {
+    code = "remove_tina_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_torgue_checks =
+  {
+    code = "remove_torgue_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_scarlett_checks =
+  {
+    code = "remove_scarlett_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_hammerlock_checks =
+  {
+    code = "remove_hammerlock_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_digi_peak_checks =
+  {
+    code = "remove_digi_peak_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_headhunter_checks =
+  {
+    code = "remove_headhunter_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_base_game_checks =
+  {
+    code = "remove_base_game_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  },
+  remove_raidboss_checks =
+  {
+    code = "remove_raidboss_checks",
+    mapping =
+    {
+      [0] = 0, -- Keep
+      [1] = 1  -- Remove
+    }
+  }
+}
