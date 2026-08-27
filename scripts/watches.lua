3674691cf9c7edc2f84174e88c53cc22a085a8e9
DDogeOneeSama <ddogcoolthing@gmail.com>
2026-08-26 23:06:14 -0500

    Backpack icon correctly shows backpack space

--- scripts/watches.lua
+++ scripts/watches.lua
@@ -6,3 +6,21 @@ Archipelago:AddLocationHandler("location handler", onLocation)
 Archipelago:AddSetReplyHandler("notify handler", OnNotify)
 Archipelago:AddRetrievedHandler("notify launch handler", OnNotifyLaunch)
 ScriptHost:AddWatchForCode("state changed", "*", InvalidateAccessibleRegions)
+
+function BackpackOverlayUpdate(code)
+    local backpack = Tracker:FindObjectForCode("backpackupgrade") or ""
+    local upgrades = backpack.CurrentStage or 0
+    local amount = 12 + (upgrades*3)
+
+    backpack:SetOverlayFontSize(16)
+    backpack:SetOverlayColor("#ffffff")
+
+    if (upgrades == 10) or (code == "infiniteupgrade") then
+        backpack:SetOverlay("∞")
+    elseif code == "backpackupgrade" then
+        backpack:SetOverlay(tostring(amount))
+    end
+end
+
+ScriptHost:AddWatchForCode("backpack update", "backpackupgrade", BackpackOverlayUpdate)
+ScriptHost:AddWatchForCode("backpack update infinite", "infiniteupgrade", BackpackOverlayUpdate)
